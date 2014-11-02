55d20543691a2f9b706c76fb392176dd01c88c61
Amadornes <amadornes@gmail.com>
2014-11-02 19:14:32 +0100

    *Attempt* to fix maven

--- src/main/java/com/amadornes/framez/ref/ModInfo.java
+++ src/main/java/com/amadornes/framez/ref/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo {
 
     public static final String MODID = "framez";
     public static final String NAME = "Framez";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "@VERSION@";
 
     public static final String GUI_FACTORY = "com.amadornes." + MODID + ".client.gui.GuiFactory";
 
